05f0b2d2b8f91ba039cd8df29ae11f644267085b
Elisabeth Fillmore <Elisabeth Fillmore>
2016-07-20 20:33:24 -0700

    assignment 6 finished

--- assig5.js
+++ assig5.js
@@ -1,45 +1,154 @@
 var pioneerSquare = {
-        minCustomer: 17,
-        maxCustomer: 88,
-        avgCS: 5.2,
+    minCustomer: 17,
+    maxCustomer: 88,
+    avgCS: 5.2,
 
-        randomCustomerPerHour: function(maxCustomer, minCustomer) {
-            return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
-        },
+    randomCustomerPerHour: function(maxCustomer, minCustomer) {
+        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
+    },
 
-        cookiesNeed: function(rcpH, avgCS) {
-            var cNeed = this.randomCustomerPerHour() * this.avgCS;
-            return Math.ceil(cNeed)
-        },
+    cookiesNeed: function(rcpH, avgCS) {
+        var cNeed = this.randomCustomerPerHour() * this.avgCS;
+        return Math.ceil(cNeed)
+    },
 
-        thirdFunction: function() {
-            var listValues = new Array(0);
-            for (var i = 10; i < 19; i++) {
-                listValues.push(this.cookiesNeed());
-              }
-          return listValues
-        },
+    thirdFunction: function() {
+        var listValues = new Array(0);
+        for (var i = 0; listValues.length < 9; i++) {
+            listValues.push(this.cookiesNeed());
+            document.getElementById("psq").innerHTML += "<td>" + listValues[i] + "</td>"
+        }
+        var total = listValues.reduce(add, 0);
 
-      //
-      //   lastFunction: function() {
-      //     while (var i = 0; i <= 8; i++) {
-      //       console.log()
-      //
-      //     }
-      // }
+        function add(a, b) {
+            return a + b;
+        }
+        document.getElementById("psq").innerHTML += "<td> Total =" + total + "</td>"
+
     }
-    document.getElementById("psq");
-    document.innerHTML += "<td>"+pioneerSquare.cookiesNeed()+"</td>"
+}
+pioneerSquare.thirdFunction()
 
-    // var pdx = {
-    //     minCustomer: 6;maxCustomer: 24;avgCS: 1.2
-    // }
-    // var washingtonSq = {
-    //     minCustomer: 11;maxCustomer: 3.8;avgCS: 1.9
-    // }
-    // var sellwood = {
-    //     minCustomer: 20;maxCustomer: 48;avgCS: 3.3
-    // }
-    // var pearlDistrict = {
-    //     minCustomer: 3;maxCustomer: 24;avgCS: 2.6
-    // }
+var pDX = {
+    minCustomer: 6,
+    maxCustomer: 24,
+    avgCS: 1.2,
+
+    randomCustomerPerHour: function(maxCustomer, minCustomer) {
+        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
+    },
+
+    cookiesNeed: function(rcpH, avgCS) {
+        var cNeed = this.randomCustomerPerHour() * this.avgCS;
+        return Math.ceil(cNeed)
+    },
+
+    thirdFunction: function() {
+        var listValues = new Array(0);
+        for (var i = 0; listValues.length < 9; i++) {
+            listValues.push(this.cookiesNeed());
+            document.getElementById("pdx").innerHTML += "<td>" + listValues[i] + "</td>"
+        }
+        var total = listValues.reduce(add, 0);
+
+        function add(a, b) {
+            return a + b;
+        }
+        document.getElementById("pdx").innerHTML += "<td> Total =" + total + "</td>"
+
+    }
+}
+pDX.thirdFunction()
+
+var washingtonSq = {
+    minCustomer: 11,
+    maxCustomer: 38,
+    avgCS: 1.9,
+
+    randomCustomerPerHour: function(maxCustomer, minCustomer) {
+        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
+    },
+
+    cookiesNeed: function(rcpH, avgCS) {
+        var cNeed = this.randomCustomerPerHour() * this.avgCS;
+        return Math.ceil(cNeed)
+    },
+
+    thirdFunction: function() {
+        var listValues = new Array(0);
+        for (var i = 0; listValues.length < 9; i++) {
+            listValues.push(this.cookiesNeed());
+            document.getElementById("wsq").innerHTML += "<td>" + listValues[i] + "</td>"
+        }
+        var total = listValues.reduce(add, 0);
+
+        function add(a, b) {
+            return a + b;
+        }
+        document.getElementById("wsq").innerHTML += "<td> Total =" + total + "</td>"
+
+    }
+}
+washingtonSq.thirdFunction()
+
+var sellwood = {
+    minCustomer: 20,
+    maxCustomer: 48,
+    avgCS: 3.3,
+
+    randomCustomerPerHour: function(maxCustomer, minCustomer) {
+        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
+    },
+
+    cookiesNeed: function(rcpH, avgCS) {
+        var cNeed = this.randomCustomerPerHour() * this.avgCS;
+        return Math.ceil(cNeed)
+    },
+
+    thirdFunction: function() {
+        var listValues = new Array(0);
+        for (var i = 0; listValues.length < 9; i++) {
+            listValues.push(this.cookiesNeed());
+            document.getElementById("sw").innerHTML += "<td>" + listValues[i] + "</td>"
+        }
+        var total = listValues.reduce(add, 0);
+
+        function add(a, b) {
+            return a + b;
+        }
+        document.getElementById("sw").innerHTML += "<td> Total =" + total + "</td>"
+
+    }
+}
+sellwood.thirdFunction()
+
+var pearlDistrict = {
+    minCustomer: 3,
+    maxCustomer: 24,
+    avgCS: 2.6,
+
+    randomCustomerPerHour: function(maxCustomer, minCustomer) {
+        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
+    },
+
+    cookiesNeed: function(rcpH, avgCS) {
+        var cNeed = this.randomCustomerPerHour() * this.avgCS;
+        return Math.ceil(cNeed)
+    },
+
+    thirdFunction: function() {
+        var listValues = new Array(0);
+        for (var i = 0; listValues.length < 9; i++) {
+            listValues.push(this.cookiesNeed());
+            document.getElementById("pd").innerHTML += "<td>" + listValues[i] + "</td>"
+        }
+        var total = listValues.reduce(add, 0);
+
+        function add(a, b) {
+            return a + b;
+        }
+        document.getElementById("pd").innerHTML += "<td> Total =" + total + "</td>"
+
+    }
+}
+pearlDistrict.thirdFunction()
